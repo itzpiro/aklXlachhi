939aed1c1220bd3dc39387ee3c945fb6b1888a00
itzpiro <ayushqw4469@gmail.com>
2026-08-10 12:26:49 +0530

    pass

--- src/components/SecretCodeCard.jsx
+++ src/components/SecretCodeCard.jsx
@@ -19,7 +19,7 @@ export default function SecretCodeCard({ onCorrect }) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (code === 'I LOVE YOU300') {
+        if (code === 'I MISS YOU300') {
             onCorrect()
         } else {
             setError("Oops! That’s not the right code.")
